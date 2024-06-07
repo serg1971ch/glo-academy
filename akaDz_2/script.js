3daad58be82b0c92b09f356c4caa8ccdf5c188c9
Сергей <34056892+serg1971ch@users.noreply.github.com>
2024-06-08 02:05:51 +0300

    functions

--- akaDz_2/script.js
+++ akaDz_2/script.js
@@ -1,30 +1,49 @@
 'use strict';
 
-const title= prompt('Как называется ваш проект?', 'Dz #03');
-console.log(" ");
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 45;
+let adaptive = true;
+let title = "";
+title = prompt('Как называется ваш проект?', 'Homework Number-03');
+screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+screenPrice = prompt('Сколько будет стоить данная работа?', 12000);
+adaptive = confirm('Нужен ли адаптив на сайте?');
 
-const screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-console.log(" ");
+let rollback = 10;
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
 
-let screenPrice = prompt('Сколько будет стоить данная работа?', 12000);
-console.log(" ");
-
-const adaptive = confirm('Нужен ли адаптив на сайте?');
-console.log(" ");
-
-const service1 = prompt('Какой дополнительный тип услуги нужен?');
-const servicePrice1 = parseInt(service1);
-const service2 = prompt('Какой дополнительный тип услуги нужен?');
-const servicePrive2 = parseInt(service2);
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = parseInt(service1);
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = parseInt(service2);
 screenPrice = parseInt(screenPrice);
-let fullPrice = servicePrice1 + servicePrive2 + screenPrice;
-console.log(fullPrice);
 
-let rollback = 72;
-rollback = fullPrice * (rollback / 100);
-let servicePercentPrice = fullPrice - rollback;
-// alert(Math.ceil(servicePercentPrice));
-console.log(Math.ceil(servicePercentPrice));
+const getAllServicePrices = function () {
+    return servicePrice1 + servicePrice2;
+}
+
+const getFullPrice = function () {
+    return screenPrice + servicePrice1 + servicePrice2;
+}
+
+const showTypeOf = function (variable) {
+    console.log(variable, typeof variable);
+}
+
+const getServicePercentPrices = function () {
+    return fullPrice - (fullPrice * (rollback / 100));
+}
+
+function getTitle() {
+    return title.trim()[0].toUpperCase() + title.trim().substr(1).toLowerCase();
+}
+
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrices();
+title = getTitle();
 
 if (fullPrice > 30000) {
     console.log('Даем скидку в 10%');
@@ -35,4 +54,13 @@ if (fullPrice > 30000) {
 } else if (fullPrice < 0) {
     console.log('Что то пошло не так?');
 }
+
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
+
+console.log("allServicePrices ", allServicePrices);
+console.log("fullPrice ", fullPrice);
+console.log("servicePercentPrice ", servicePercentPrice);
+console.log("title ", title);
 
