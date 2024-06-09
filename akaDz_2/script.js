31b0bfec14a44f725d04b932e2cc00fd7058ab26
Сергей <34056892+serg1971ch@users.noreply.github.com>
2024-06-09 03:14:29 +0300

    cicles

--- akaDz_2/script.js
+++ akaDz_2/script.js
@@ -1,13 +1,9 @@
 'use strict';
 
-let screens = "Простые, Сложные, Интерактивные";
+llet screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 45;
 let adaptive = true;
 let title = "";
-title = prompt('Как называется ваш проект?', 'Homework Number-03');
-screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-screenPrice = prompt('Сколько будет стоить данная работа?', 12000);
-adaptive = confirm('Нужен ли адаптив на сайте?');
 
 let rollback = 10;
 let allServicePrices;
@@ -20,30 +16,63 @@ let service2 = prompt('Какой дополнительный тип услуг
 let servicePrice2 = parseInt(service2);
 screenPrice = parseInt(screenPrice);
 
-const getAllServicePrices = function () {
-    return servicePrice1 + servicePrice2;
+const showTypeOf = function (variable) {
+    console.log(variable, typeof variable);
+}
+
+const isNumber = function (num) {
+    return !NaN(parseFloat(num)& isFinite(num))
 }
 
 const getFullPrice = function () {
     return screenPrice + servicePrice1 + servicePrice2;
 }
 
-const showTypeOf = function (variable) {
-    console.log(variable, typeof variable);
-}
-
 const getServicePercentPrices = function () {
-    return fullPrice - (fullPrice * (rollback / 100));
+    return fullPrice - (fullPrice*(rollback/100));
 }
-
 function getTitle() {
     return title.trim()[0].toUpperCase() + title.trim().substr(1).toLowerCase();
 }
 
+const asking = function () {
+    title = prompt('Как называется ваш проект?', 'Homework Number-05');
+    screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+    do {
+        screenPrice = prompt('Сколько будет стоить данная работа?', 12000);
+    } while (isNumber(screenPrice)) {
+        adaptive = confirm('Нужен ли адаптив на сайте?');
+    }
+}
+
+const getAllServicePrices = function () {
+    let sum = 0;
+    for (let i = 0; i < 2; i++) {
+        let price = 0;
+
+        if (i === 0) {
+            service1 = prompt("Какой дополнительный тип услуги нужен?")
+        } else if (i === 1) {
+            service2 = prompt("Какой дополнительный тип услуги нужен?")
+        }
+
+        do {
+            price = promt("Сколько это будет стоить,")
+        } while (!isNumber(price))
+        sum += +price;
+    }
+    return sum;
+}
+
+asking();
 allServicePrices = getAllServicePrices();
 fullPrice = getFullPrice();
 servicePercentPrice = getServicePercentPrices();
 title = getTitle();
+
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
 
 if (fullPrice > 30000) {
     console.log('Даем скидку в 10%');
@@ -54,10 +83,6 @@ if (fullPrice > 30000) {
 } else if (fullPrice < 0) {
     console.log('Что то пошло не так?');
 }
-
-showTypeOf(title);
-showTypeOf(fullPrice);
-showTypeOf(adaptive);
 
 console.log("allServicePrices ", allServicePrices);
 console.log("fullPrice ", fullPrice);
